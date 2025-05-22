e774f175f420c691b9aeec11947ef1bb3f4f3d4b
Doruk <dorukgungor2002@gmail.com>
2025-05-22 13:17:50 +0200

    clean up and finish 2c

--- chow-liu-tree.py
+++ chow-liu-tree.py
@@ -3,6 +3,8 @@ from scipy.special import logsumexp
 import numpy as np
 import itertools
 import csv
+import networkx as nx
+import matplotlib.pyplot as plt
 
 class BinaryCLT:
 
@@ -21,40 +23,64 @@ class BinaryCLT:
 
     def _learn_structure(self):
         """
-        Learn tree structure using the Chow-Liu algorithm.
+        Learn tree structure using the Chow-Liu algorithm:
+        1. Compute mutual information matrix
+        2. Build MST from negative mutual information
+        3. Use breadth-first order to determine parent relationships
         """
-        # TODO: Compute mutual information matrix
-        # TODO: Build MST from negative mutual information
-        # TODO: Use breadth-first order to set parents
-        pass
+        mi_matrix = np.zeros((self.d, self.d))
+
+        # Compute pairwise mutual information
+        for i in range(self.d):
+            for j in range(i + 1, self.d):
+                counts = np.zeros((2, 2))
+                for k in range(self.n):
+                    xi = int(self.data[k, i])
+                    xj = int(self.data[k, j])
+                    counts[xi, xj] += 1
+                counts += self.alpha  # Laplace smoothing
+                probs = counts / counts.sum()
+
+                mi = 0.0
+                for xi in [0, 1]:
+                    for xj in [0, 1]:
+                        p_ij = probs[xi, xj]
+                        p_i = probs[xi, :].sum()
+                        p_j = probs[:, xj].sum()
+                        if p_ij > 0:
+                            mi += p_ij * np.log(p_ij / (p_i * p_j))
+
+                mi_matrix[i, j] = mi_matrix[j, i] = mi
+
+        # Build MST from negative mutual information
+        neg_mi = -mi_matrix
+        mst = minimum_spanning_tree(neg_mi).toarray()
+
+        # Use BFS to determine parents in tree
+        _, parents = breadth_first_order(mst + mst.T, directed=False, i_start=self.root, return_predecessors=True)
+        self.tree = [-1 if i == self.root else parents[i] for i in range(self.d)]
 
     def _learn_parameters(self):
-        """
-        Learn CPTs using Laplace smoothing and convert to log domain.
-        """
-        # TODO: Count co-occurrences with Laplace correction
-        # TODO: Store log CPTs in self.log_params
         self.log_params = []
         for i in range(self.d):
             if i == self.root:
-                # Root node has no parent 
-                counts = np.zeros((2, 1))
+                counts = np.zeros((2,))
                 for j in range(self.n):
-                    counts[self.data[j, i]] += 1
-                # Laplace smoothing
+                    xi = int(self.data[j, i])
+                    counts[xi] += 1
                 counts += 2 * self.alpha
-                # Change shape to (2, 2) for consistency
-                counts = np.repeat(counts[:, np.newaxis], 2, axis=1)
+                probs = counts / counts.sum()
+                log_prob = np.log(probs)
+                log_prob = np.tile(log_prob[None, :], (2, 1))  # (2,2)
             else:
                 parent = self.tree[i]
-                # Count co-occurrences
                 counts = np.zeros((2, 2))
                 for j in range(self.n):
-                    counts[self.data[j, parent], self.data[j, i]] += 1
-                # Laplace smoothing
+                    pi = int(self.data[j, parent])
+                    xi = int(self.data[j, i])
+                    counts[pi, xi] += 1
                 counts += self.alpha
-            # Convert to log domain
-            log_prob = np.log(counts / (4 * self.alpha + counts.sum(axis=1, keepdims=True)))
+                log_prob = np.log(counts / counts.sum(axis=1, keepdims=True))
             self.log_params.append(log_prob)
         return self.log_params
 
@@ -66,24 +92,64 @@ class BinaryCLT:
 
     def get_log_params(self):
         """
-        Return the log CPTs.
+        Efficiently return the learned log CPTs as a (d, 2, 2) NumPy array.
+        Assumes _learn_parameters has already populated self.log_params.
         """
-        return self.log_params
+        return np.stack(self.log_params, axis=0)
+
 
     def log_prob(self, x, exhaustive: bool = False):
         """
         Compute log-probability of observed/marginalized samples.
+        
+        Parameters:
+            x (ndarray): N x D matrix where rows are binary vectors (0/1) with optional np.nan
+            exhaustive (bool): Whether to perform exhaustive marginalization over missing values
+        
+        Returns:
+            log_probs (ndarray): N x 1 vector of log-probabilities
         """
         lp = []
         for query in x:
             if exhaustive:
-                # TODO: Exhaustive inference over missing values
-                pass
-            else:
-                # TODO: Efficient inference using variable elimination
-                pass
-        return np.array(lp)
+                # Identify missing indices
+                missing_indices = np.where(np.isnan(query))[0]
 
+                # List to collect log-probs of all completions
+                log_probs = []
+
+                # Try all combinations of values for missing variables
+                for values in itertools.product([0, 1], repeat=len(missing_indices)):
+                    filled = query.copy()
+                    filled[missing_indices] = values
+                    logp = 0.0
+                    for i in range(self.d):
+                        xi = int(filled[i])
+                        parent = self.tree[i]
+                        if parent == -1:
+                            logp += self.log_params[i][0, xi]  # root: marginal prob
+                        else:
+                            pi = int(filled[parent])
+                            logp += self.log_params[i][pi, xi]
+                    log_probs.append(logp)
+
+                lp.append(logsumexp(log_probs))
+
+            else:
+                # Assume all values observed
+                logp = 0.0
+                for i in range(self.d):
+                    xi = int(query[i])
+                    parent = self.tree[i]
+                    if parent == -1:
+                        logp += self.log_params[i][0, xi]
+                    else:
+                        pi = int(query[parent])
+                        logp += self.log_params[i][pi, xi]
+                lp.append(logp)
+
+        return np.array(lp).reshape(-1, 1)
+    
     def sample(self, n_samples: int):
         """
         Generate i.i.d. samples from the CLT distribution using ancestral sampling.
@@ -112,3 +178,28 @@ def load_csv_dataset(filename):
         reader = csv.reader(file, delimiter=',')
         dataset = np.array(list(reader)).astype(np.float_)
     return dataset
+
+
+def plot_tree(tree):
+    G = nx.DiGraph()
+    for child, parent in enumerate(tree):
+        if parent != -1:
+            G.add_edge(parent, child)
+    pos = nx.spring_layout(G, seed=42)
+    nx.draw(G, pos, with_labels=True, node_size=500, node_color="lightblue", arrows=True)
+    plt.title("Chow-Liu Tree Structure")
+    plt.show()
+
+
+nltcs_data = load_csv_dataset("nltcs.train.data")
+
+model_nltcs = BinaryCLT(nltcs_data, root=0, alpha=0.01)
+# Get tree and CPTs
+tree_structure = model_nltcs.get_tree()
+log_cpts = model_nltcs.get_log_params()
+
+print("\n--- Tree Structure (Parent List) ---")
+print(tree_structure)
+
+print("\n--- Log CPTs (shape:", log_cpts.shape, ") ---")
+print(log_cpts)
